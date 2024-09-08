ae4b7215643de3ca5619113c46dd824a16c8fc07
WiseCraftMedia <wisecraftmedia@gmail.com>
2024-09-08 00:10:46 -0400

    flashy buttons

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
-
-document.addEventListener("keydown", function (e) {
-  makeSound(e.key);
+document.addEventListener("keydown", function (ev) {
+  makeSound(ev.key);
+  buttonAnimation(ev.key);
 });
-
 function makeSound(key) {
   switch (key) {
     case "w":
@@ -52,45 +52,12 @@ function makeSound(key) {
   }
 }
 
-console.log(makeSound);
+function buttonAnimation(key) {
+  var activeButton = document.querySelector("." + key);
 
-// document.addEventListener("keydown", function (e) {
-//   switch (e.key) {
-//     case "w":
-//       var tom1 = new Audio("./sounds/tom-1.mp3");
-//       tom1.play();
-//       break;
+  activeButton.classList.add("pressed");
 
-//     case "a":
-//       var tom2 = new Audio("./sounds/tom-2.mp3");
-//       tom2.play();
-//       break;
-
-//     case "s":
-//       var tom3 = new Audio("./sounds/tom-3.mp3");
-//       tom3.play();
-//       break;
-
-//     case "d":
-//       var tom4 = new Audio("./sounds/tom-4.mp3");
-//       tom4.play();
-//       break;
-
-//     case "j":
-//       var snare = new Audio("./sounds/snare.mp3");
-//       snare.play();
-//       break;
-
-//     case "k":
-//       var crash = new Audio("./sounds/crash.mp3");
-//       crash.play();
-//       break;
-
-//     case "l":
-//       var kickBass = new Audio("./sounds/kick-bass.mp3");
-//       kickBass.play();
-//       break;
-//     default:
-//       console.log(e);
-//   }
-// });
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
